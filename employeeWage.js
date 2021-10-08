7e547e9786bf89ce378c336e3ae6ce90809f5f20
mkaubr007 <mkaubr007@gmail.com>
2021-10-08 14:43:38 +0530

    NumberOfWorkingDays added

--- employeeWage.js
+++ employeeWage.js
@@ -3,6 +3,7 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOURS=20;
+const NUM_OF_WORKING_DAYS=2;
 let empHrs=0; 
 let empCheck=Math.floor(Math.random()*10)%3;
 function getWorkingHours(empCheck){
@@ -17,5 +18,9 @@ function getWorkingHours(empCheck){
             empHrs=0;
     }
 }
+for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
+    let empCheck=Math.floor(Math.random()*10)%3;
+    empHrs+=getWorkingHours(empCheck);
+}
 let empWage=empHrs*WAGE_PER_HOURS;
-console.log("Emp Wage: "+empWage);
+console.log("Total Hrs: "+empHrs+" Emp Wage: "+empWage);
