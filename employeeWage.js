eb81d1a6750ff4fcdb1983bf16720a9f3c5f1fd6
mkaubr007 <mkaubr007@gmail.com>
2021-10-08 15:07:12 +0530

    TotalDaysAndHoursInMonth added

--- employeeWage.js
+++ employeeWage.js
@@ -3,7 +3,10 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOURS=20;
-const NUM_OF_WORKING_DAYS=2;
+const NUM_OF_WORKING_DAYS=10;
+const MAX_HRS_IN_MONTH=100;
+let totalEmpHrs=160;
+let totalWorkingDays=20;
 let empHrs=0; 
 let empCheck=Math.floor(Math.random()*10)%3;
 function getWorkingHours(empCheck){
@@ -22,5 +25,10 @@ for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
     let empCheck=Math.floor(Math.random()*10)%3;
     empHrs+=getWorkingHours(empCheck);
 }
-let empWage=empHrs*WAGE_PER_HOURS;
-console.log("Total Hrs: "+empHrs+" Emp Wage: "+empWage);
+while(totalEmpHrs<MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let emCheck=Math.floor(Math.random()*10)%3;
+    totalEmpHrs+=getWorkingHours(empCheck);
+}
+let empWage=totalEmpHrs*WAGE_PER_HOURS;
+console.log("Total Days: "+totalWorkingDays+" Total Hrs: "+totalEmpHrs+" Emp Wage"+empWage);
